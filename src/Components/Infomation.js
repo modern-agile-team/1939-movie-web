a6202532f2733067831d43deace61a77355fd063
Min <alstnsrl98@gmail.com>
2021-11-20 11:31:49 +0900

    Modify(soonki/information): 포스터/시놉 UI 일부 수정

--- src/Components/Infomation.js
+++ src/Components/Infomation.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  height: 100vh;
-  justify-content: center;
-`;
-
-const Title = styled.h1`
-  margin: 0;
-  padding-top: 2rem;
-  text-align: center;
-`;
-
 const Poster = styled.h1`
   width: 25vw;
-  height: 60vh;
+  height: 100%;
   text-align: center;
   background-color: lightgray;
   border-radius: 6px;
@@ -22,38 +11,34 @@ const Poster = styled.h1`
 
 const Sinop = styled.div`
   width: 60vw;
-  height: 50vh;
+  height: 100%;
   text-align: center;
+  background-color: lightgray;
 `;
 
 const FlexDiv = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  margin-top: 4em;
+  margin: 100px;
+  height: 80vh;
 `;
 
 export const Infomation = () => {
   return (
-    <div>
-      <Container>
-        <Title>19인 너 39에 난</Title>
-        <FlexDiv>
-          <Poster>포스터</Poster>
-          <Sinop>
-            <h1>SYNOPSIS</h1>
-            <h3>줄거리</h3>
-            <article>
-              19인 넌 39인 난에서 선보이는 작품은 독특한 이야기, 뛰어난 촬영
-              기술, 독보적인 편집 기술로 이미 정평이 나 있습니다. 수많은 상을
-              받은 유명 배우와 뛰어난 스태프가 뭉쳤습니다. 이 작품만큼은
-              기대하셔도 좋습니다. 독점 예고편을 비롯한 관련 정보를
-              정리하였습니다. 19인 넌 39인 난에 대해 궁금하신 점은 홈페이지를
-              참고해주세요.
-            </article>
-          </Sinop>
-        </FlexDiv>
-      </Container>
-    </div>
+    <FlexDiv>
+      <Poster>포스터</Poster>
+      <Sinop>
+        <h1>SYNOPSIS</h1>
+        <h3>줄거리</h3>
+        <article>
+          19인 넌 39인 난에서 선보이는 작품은 독특한 이야기, 뛰어난 촬영 기술,
+          독보적인 편집 기술로 이미 정평이 나 있습니다. 수많은 상을 받은 유명
+          배우와 뛰어난 스태프가 뭉쳤습니다. 이 작품만큼은 기대하셔도 좋습니다.
+          독점 예고편을 비롯한 관련 정보를 정리하였습니다. 19인 넌 39인 난에
+          대해 궁금하신 점은 홈페이지를 참고해주세요.
+        </article>
+      </Sinop>
+    </FlexDiv>
   );
 };
